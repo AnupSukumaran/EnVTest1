7a42f928ec0bc07ace7377ae2ee87938f108ddba
AnupSukumaran <anup.sukumaran9@gmail.com>
2025-04-23 17:14:02 +0100

    Initial Commit

---
+++ EnVTest1/EnVTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  EnVTest1App.swift
+//  EnVTest1
+//
+//  Created by Sukumar.Sukumaran on 23/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct EnVTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
